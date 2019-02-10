fdd2c29530784240a6a5cd030732262a30a8d999
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-02-11 08:17:11 +0900

    :wrench: add config to eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,23 @@ module.exports = {
   parser: "typescript-eslint-parser",
   extends: ["airbnb", "plugin:prettier/recommended"],
   plugins: ["react", "typescript"],
+  globals: {
+    __DEV__: true
+  },
+  env: {
+    jest: true
+  },
   rules: {
-    "react/jsx-filename-extension": [2, { extensions: [".tsx", ".ts"] }]
+    "react/jsx-filename-extension": [2, { extensions: [".tsx", ".ts"] }],
+    // use custom non-unused-vars for typescript
+    "no-unused-vars": "off",
+    "typescript/no-unused-vars": ["error"]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".ts", ".tsx"]
+      }
+    }
   }
 };
