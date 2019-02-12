638b1c30fd760f67baae2376970837b1efea7bf9
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-02-13 01:38:59 +0900

    :sparkles: add minimal redux

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import reduxLogger from "redux-logger";
+import reducer from "../reducer";
+
+// development なら redux-loggerを入れる
+const logger = __DEV__ ? [reduxLogger] : [];
+
+const middlewares = applyMiddleware(thunk, ...logger);
+
+const initialState = {};
+
+const store = createStore(reducer, initialState, middlewares);
+const persistor = persistStore(store);
+
+export { store, persistor };
