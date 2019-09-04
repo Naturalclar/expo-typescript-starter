0a82abd075d979c954bb609f7ea83088ef85270d
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-09-04 19:20:57 +0900

    feature/add ListItem

---
+++ src/atoms/ListItem.story.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { text } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import ListItem from "./ListItem";
+import { StorybookContainer } from "../templates/StoryContainer";
+
+storiesOf("atoms", module)
+  .addDecorator(StorybookContainer)
+  .add("ListItem", () => (
+    <ListItem
+      onPress={action("onPress")}
+      name={text("name", "Bob Dilan")}
+      content={text("content", "hello world")}
+    />
+  ));
---
+++ src/atoms/ListItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row"
+  },
+  name: { fontWeight: "bold" },
+  content: {},
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "blue",
+    marginRight: 8
+  }
+});
+
+type Props = {
+  onPress: () => void;
+  name: string;
+  content: string;
+};
+
+const ListItem: React.FC<Props> = ({
+  onPress,
+  name,
+  content
+}: Props): JSX.Element => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles.container}>
+      <View style={styles.avatar} />
+      <View>
+        <Text style={styles.name}>{name}</Text>
+        <Text>{content}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+export default ListItem;
