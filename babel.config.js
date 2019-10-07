4cd50549922c67b93464208453b73a89a8681db4
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-10-07 14:23:22 -0300

    feat: upgrade packages, support styled-components and module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      'react-native-paper/babel',
+      "babel-plugin-styled-components",
+      [
+        "module-resolver",
+        {
+          "root": ["./src"],
+          "alias": {
+            'react-native-vector-icons': '@expo/vector-icons',
+            "src": "./src",
+            "assets": "./assets",
+            "config": "./src/config",
+            "locales": "./src/locales",
+            "helpers": "./src/helpers",
+            "containers": "./src/containers",
+            "components": "./src/components",
+            "screens": "./src/screens",
+            "store": "./src/store",
+            "theme": "./src/theme"
+          }
+        }
+      ]
+    ],
+  }
+}
