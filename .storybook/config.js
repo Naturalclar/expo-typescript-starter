95500cd3acef36ac12ae3b0f16bd74c79bcdd104
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-10-07 14:25:04 -0300

    feat: storybook with support to styled components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,23 +1,34 @@
-import { configure, addDecorator, addParameters } from "@storybook/react";
-import { withOptions } from "@storybook/addon-options";
-import { withKnobs } from "@storybook/addon-knobs";
+import { configure, addDecorator, addParameters } from "@storybook/react"
+import { setOptions } from "@storybook/addon-options"
+import { withKnobs } from "@storybook/addon-knobs"
+import { withThemesProvider } from 'storybook-addon-styled-component-theme'
+import { themes } from '../src/theme'
+import '../src/locales'
+const App = require('../app.json')
 
-function loadStories() {
-  const req = require.context("../src", true, /\.story\.tsx?$/);
-  req.keys().forEach(story => req(story));
-}
-
-addDecorator(withKnobs({ escapeHTML: false }));
-addDecorator(
-  withOptions({
-    addonPanelInRight: true
-  })
-);
+setOptions({
+  name: App.name,
+  url: 'https://necolas.github.io/react-native-web',
+  goFullScreen: false,
+  addonPanelInRight: false,
+  showSearchBox: false,
+  showAddonPanel: true,
+  showStoriesPanel: true
+})
 
 addParameters({
   viewport: {
-    defaultViewport: "iphone5"
+    defaultViewport: "iphone6"
   }
-});
+})
 
-configure(loadStories, module);
+addDecorator(withThemesProvider(themes))
+
+addDecorator(withKnobs({ escapeHTML: false }))
+
+const loadStories = () => {
+  const req = require.context("../src", true, /\.story\.tsx?$/)
+  req.keys().forEach(story => req(story))
+}
+
+configure(loadStories, module)
