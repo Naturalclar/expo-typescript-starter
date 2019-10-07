dfe824e6833c0210e6210dce0dd92d7ae7ebe6be
Willian Ribeiro Angelo <agfoccus@gmail.com>
2019-10-07 14:25:41 -0300

    feat: multi language with react-i18next

---
+++ src/locales/index.tsx
@@ -0,0 +1,24 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+const resources = {
+  en: {
+    translation: require('./en.json')
+  },
+  pt: {
+    translation: require('./pt.json')
+  }
+}
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'en'
+
+    // keySeparator: false, // we do not use keys in form messages.welcome
+
+    // interpolation: {
+    //   escapeValue: false // react already safes from xss
+    // }
+  })
